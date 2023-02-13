2f533a6e223b3abfaad2fea6b444deea182dde6d
Pham Khanh Hung <hungpk1412@gmail.com>
2023-02-13 15:01:51 +0700

    added vpc

---
+++ 03-demo-ec2/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "vpc_linux" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "Linux"
+  }
+}
